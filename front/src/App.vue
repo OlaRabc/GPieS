8036ffe299b8589187903c46f1e27a4ecd5ee109
Ola R <olar8871@gmail.com>
2023-01-05 18:20:02 +0100

    vue fixes

---
+++ front/src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import Soon from './components/Soon.vue'
+</script>
+
+<template>
+   <div> NAV</div>
+
+    <div class="wrapper">
+      <Soon/>
+    </div>
+
+    <footer>
+      footer
+    </footer>
+</template>
+
+<style scoped>
+
+</style>
---
+++ front/src/components/Soon.vue
@@ -0,0 +1,14 @@
+<template>
+   <h1>
+    Przepraszamy, strona dopiero powstaje!
+   </h1> 
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  top: -10px;
+  /* color: white; */
+}
+</style>
