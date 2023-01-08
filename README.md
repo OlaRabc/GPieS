76cb7e989f4a1763c8688446f8adb73a2d08d065
Ola R <olar8871@gmail.com>
2023-01-08 18:06:27 +0100

    README

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # GPieS
+# frontend - Vue.js aplication
+To open aplication you need node and npm
+
+1. Open **front** file
+2. **npm install** in terminal
+3. **npm run dev** in terminal
+4. **app** work on http://localhost:5173/
---
+++ front/src/components/Login.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="loginPage">
+    <form @submit.prevent="login">
+      <label> Username: </label>
+      <input v-model="username" type="text" />
+      <label> Password: </label>
+      <input id="password" v-model="password" type="password" />
+
+      <button type="submit">Log in</button>
+    </form>
+  </div>
+</template>
+
+<style scoped>
+.loginPage {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  font-family: "Nunito", sans-serif;
+  margin: 128px 0px;
+}
+form {
+  border: 1px solid black;
+  border-radius: 5px;
+  padding: 128px 16px;
+  width: 50%;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+
+  box-shadow: 12px 12px 2px 1px #b3bcc7;
+}
+input {
+  margin-bottom: 16px;
+}
+button {
+  background: #e1ecfa;
+  color: #495f7a;
+  border: none;
+  border-radius: 5px;
+  padding: 8px 32px;
+  transition: color 0.75s, background-color 0.75s;
+  font-weight: 700;
+  font-size: 1.2rem;
+}
+button:hover {
+  background: #96c2fb;
+  color: #495f7a;
+  transition: color 0.75s, background-color 0.75s;
+}
+</style>
+<script>
+export default {
+  data() {
+    return {
+      username: "",
+      password: "",
+    };
+  },
+  methods: {
+    login() {
+      // Make an HTTP request to authenticate the user's credentials
+      // and set the login state
+    },
+  },
+};
+</script>
